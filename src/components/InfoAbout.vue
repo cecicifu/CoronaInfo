a04904f5602d9eca0998f32d38fdb84d8191102d
SrCifu <ceciliocifuentes@gmail.com>
2020-09-18 17:38:39 +0200

    Report way

--- src/components/InfoAbout.vue
+++ src/components/InfoAbout.vue
@@ -2,10 +2,7 @@
   <div class="container mt-4">
     <h1 class="mb-4">About</h1>
     <p class="lead">Web application developed to provide information to everyone about COVID-19</p>
-    <p class="lead">
-      Report an issue:
-      <a href="mailto:unknowpy@protonmail.com">unknowpy@protonmail.com</a>
-    </p>
+    <p class="lead">To report an issue, click below ;D</p>
     <a href="https://github.com/cecicifu/" target="_blank">
       <i class="fab fa-github fa-5x svg-inline--fa"></i>
     </a>
